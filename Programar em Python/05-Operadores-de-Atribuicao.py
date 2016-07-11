b71a1421e43cbabea2d5e794a5991bd02cca6b34
Nelson Silva <devwop@outlook.com>
2016-07-11 22:04:58 +0100

    [Programar em Python/05-Operadores-de-Atribuicao.py] Add

---
+++ Programar em Python/05-Operadores-de-Atribuicao.py
@@ -0,0 +1,26 @@
+# Programar em Python #05 - Operadores de Atribuição
+
+'''
+	a = a + 3 -> a += 3
+	b = b - 2 -> b -= 2
+	c = c * 2 -> c *= 2
+	d = d / 2 -> d /= 2
+	e = e % 2 -> e %= 2
+	f = f ** 2 -> f **= 2
+'''
+
+a = b = c = d = e = f = 2;
+
+a += 3;
+b -= 2;
+c *= 2;
+d /= 2;
+e %= 2;
+f **= 2;
+
+print('Valor de a:', a);
+print('Valor de b:', b);
+print('Valor de c:', c);
+print('Valor de d:', d);
+print('Valor de e:', e);
+print('Valor de f:', f);
