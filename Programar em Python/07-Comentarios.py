0119ec50140d00ac1cf6a8b91f1ce39f7bd52eef
Nelson Silva <devwop@outlook.com>
2016-07-13 18:27:06 +0100

    [Programar em Python/07-Comentarios.py] Add

---
+++ Programar em Python/07-Comentarios.py
@@ -0,0 +1,15 @@
+# Programar em Python #07 - Comentários
+
+'''
+	Os comentários (de uma ou de várias linhas) servem para comentar tudo o que seja necessário para que uma outra pessoa compreenda o nosso código
+'''
+
+# Eu sou um comentário!
+
+print('Eu sou uma string!');
+
+'''
+	Eu também sou um comentário!
+	Eu faço parte deste mesmo comentário!
+	Continuo a fazer parte dele!
+'''
