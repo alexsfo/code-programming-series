f6cd24193880f8b73b4c15bbb097c78f28279b89
Nelson Silva <devwop@outlook.com>
2016-07-14 21:06:25 +0100

    [Programar em Python/08-Funcao-input.py] Add

---
+++ Programar em Python/08-Funcao-input.py
@@ -0,0 +1,14 @@
+# Programar em Python #08 - Função input()
+
+'''
+	A função input() tem como principal objetivo "extrair" informações dadas pelo utilizador
+	Resumindo, esta mesma função vai aumentar a interação com o mesmo
+'''
+
+primeiroNome = input('Insere o teu primeiro nome: ');
+ultimoNome = input('Insere o teu último nome: ');
+idade = input('Insere a tua idade: ');
+
+print('Primeiro nome:', primeiroNome);
+print('Último nome:', ultimoNome);
+print('Idade:', idade);
