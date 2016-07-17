e20aa251418019df23c5573fd4911b8607fe6620
Nelson Silva <devwop@outlook.com>
2016-07-17 20:51:50 +0100

    [Programar em Python/11-Operadores-de-Comparacao.py] Add

---
+++ Programar em Python/11-Operadores-de-Comparacao.py
@@ -0,0 +1,21 @@
+# Programar em Python #10 - Operadores de Comparação
+
+'''
+	== -> Igual
+	!= -> Diferente
+	> -> Maior
+	< -> Menor
+	>= -> Maior e igual
+	<= -> Menor e igual
+
+	and -> E
+	or -> Ou
+'''
+
+x = 10;
+y = 20;
+
+if x <= y or x == y:
+	print('Esta condição é verdadeira!');
+else:
+	print('A condição anterior é falsa!');
