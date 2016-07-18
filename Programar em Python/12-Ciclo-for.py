e9743e370bb3d68d7616eb82c8dd4614fbc7c50e
Nelson Silva <devwop@outlook.com>
2016-07-18 18:22:43 +0100

    [Programar em Python/12-Ciclo-for.py] Add

---
+++ Programar em Python/12-Ciclo-for.py
@@ -0,0 +1,20 @@
+# Programar em Python #12 - Ciclo for
+
+'''
+	Um ciclo permite executar uma instrução ou um conjunto de instruções várias vezes
+	O ciclo for dá voltas (itera) consoante os itens de uma sequência (uma lista, uma string ou um dado range)
+'''
+
+organizacao = 'Caffeine Algorithm';
+cores = ['Azul', 'Verde', 'Amarelo', 'Vermelho', 'Laranja'];
+
+'''
+	for carater in organizacao:
+		print('Carater:', carater);
+
+	for cor in cores:
+		print('Cor:', cor);
+'''
+
+for numero in range(1, 11):
+	print('Número:', numero);
