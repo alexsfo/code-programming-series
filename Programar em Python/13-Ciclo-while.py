1c98b4716fffd099bceca799d8b8882e1b8490f4
Nelson Silva <devwop@outlook.com>
2016-07-19 20:39:07 +0100

    [Programar em Python/13-Ciclo-while.py] Add

---
+++ Programar em Python/13-Ciclo-while.py
@@ -0,0 +1,12 @@
+# Programar em Python #13 - Ciclo while
+
+'''
+	Um ciclo permite executar uma instrução ou um conjunto de instruções várias vezes
+	O ciclo while dá voltas (itera) consoante uma determinada condição (tendo ela que ser verdadeira)
+'''
+
+numero = 1;
+
+while numero <= 10:
+	print('Número da volta:', numero);
+	numero += 1;
