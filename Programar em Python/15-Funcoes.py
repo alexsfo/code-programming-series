aa3278acb0ad3d95b0daaf8ad92ed46b4044ec88
Nelson Silva <devwop@outlook.com>
2016-07-21 21:03:45 +0100

    [Programar em Python/15-Funcoes.py] Add

---
+++ Programar em Python/15-Funcoes.py
@@ -0,0 +1,13 @@
+# Programar em Python #15 - Funções
+
+'''
+	Uma função é um pedaço de código organizado e reutilizável que é usado para executar uma única ação relacionada.
+	As funções proporcionam uma melhor modularidade e um alto grau de reutilização de código
+'''
+
+def dadosPessoais():
+	print('Nome: Nelson Silva');
+	print('Idade: 21');
+	print('Nacionalidade: Portuguesa');
+
+dadosPessoais();
