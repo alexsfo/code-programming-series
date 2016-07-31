69cada099229158f230270289a82b16bf85da848
Nelson Silva <devwop@outlook.com>
2016-07-31 19:01:37 +0100

    [Programar em Python/24-Modulos.py] Add

---
+++ Programar em Python/24-Modulos.py
@@ -0,0 +1,18 @@
+# Programar em Python #24 - Módulos
+
+'''
+	Os módulos são basicamente objetos com atributos que se podem ligar através de uma referência
+	Estes mesmos permitem agrupar o seu código, o que torna tudo mais fácil de entender e de usar
+
+	dir(modulo) -> Retorna uma lista ordenada de strings contendo os nomes definidos no módulo em questão
+
+	Essa mesma lista contém os nomes de todas as variáveis e funções que foram definidas nesse mesmo módulo
+'''
+
+# import math;
+# from math import sqrt;
+from math import *;
+
+# print('Conteúdo do módulo:', dir(math));
+# print('Função sqrt:', sqrt(10));
+print('Valor de pi:', pi);
