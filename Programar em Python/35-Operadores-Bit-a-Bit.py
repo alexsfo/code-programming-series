18b7bb7ad71e2932a84c452e37022a322f30a4fb
Nelson Silva <devwop@outlook.com>
2016-08-10 14:52:09 +0100

    [Programar em Python/35-Operadores-Bit-a-Bit.py] Add

---
+++ Programar em Python/35-Operadores-Bit-a-Bit.py
@@ -0,0 +1,9 @@
+# Programar em Python #35 - Operadores Bit a Bit
+
+x = 60; # 00111100
+y = 13; # 00001101
+
+print('Resultado do operador &:', x & y); # 12 -> 00001100
+print('Resultado do operador |:', x | y); # 61 -> 00111101
+print('Resultado do operador <<:', x << 2); # 240 -> 11110000
+print('Resultado do operador >>:', x >> 2); # 15 -> 00001111
