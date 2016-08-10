bea914c954b0f100629ffb5398f9758e5f5e3546
Nelson Silva <devwop@outlook.com>
2016-08-10 21:51:12 +0100

    [Programar em C#/Program.cs] Add

---
+++ Programar em C#/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Base {
+    class Program {
+        static void Main(string[] args) {
+            var video = new Arrays();
+            video.Run();
+            Console.ReadKey();
+        }
+    }
+}
