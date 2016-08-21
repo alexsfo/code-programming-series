a63f1c95c5b0fde5b8c6a6e0c885fd62f4ff2dae
Nelson Silva <devwop@outlook.com>
2016-08-21 04:41:58 +0100

    [Programar em C#/06-Operadores-de-Atribuicao.cs] Add

---
+++ Programar em C#/06-Operadores-de-Atribuicao.cs
@@ -0,0 +1,17 @@
+﻿// Programar em Python #06 - Operadores de Atribuição
+
+using System;
+
+namespace Base {
+    class OperadoresDeAtribuicao {
+        public void Run() {
+            int x = 2;
+
+            Console.WriteLine("x = x + 3: " + (x += 3));
+            Console.WriteLine("x = x - 2: " + (x -= 2));
+            Console.WriteLine("x = x * 2: " + (x *= 2));
+            Console.WriteLine("x = x / 2: " + (x /= 2));
+            Console.WriteLine("x = x % 3: " + (x %= 3));
+        }
+    }
+}
