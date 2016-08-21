2655dd20b1915d095fdfa011a1cd7c2b6db39b95
Nelson Silva <devwop@outlook.com>
2016-08-21 04:44:12 +0100

    [Programar em C#/11-Listas.cs] Add

---
+++ Programar em C#/11-Listas.cs
@@ -0,0 +1,28 @@
+﻿// Programar em Python #11 - Listas
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Base {
+    class Listas {
+        public void Run() {
+            List<string> cores = new List<string>();
+
+            cores.Add("Azul");
+            cores.Add("Verde");
+            cores.Add("Amarelo");
+            cores.Add("Vermelho");
+            cores.Add("Laranja");
+
+            cores.Remove("Laranja");
+            cores.Reverse();
+            cores.Sort();
+            cores.Clear();
+
+            Console.WriteLine($"Número de cores: {cores.Count}");
+            // Console.WriteLine($"Primeira cor: {cores.First()}");
+            // Console.WriteLine($"Última cor: {cores.Last()}");
+        }
+    }
+}
