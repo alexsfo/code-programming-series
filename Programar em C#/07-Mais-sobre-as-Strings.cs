c296c32d4e92c34ffb687cb2df3b8aae6885445b
Nelson Silva <devwop@outlook.com>
2016-08-21 04:42:32 +0100

    [Programar em C#/07-Mais-sobre-as-Strings.cs] Add

---
+++ Programar em C#/07-Mais-sobre-as-Strings.cs
@@ -0,0 +1,17 @@
+﻿// Programar em Python #07 - Mais sobre as Strings
+
+using System;
+
+namespace Base {
+    class MaisSobreAsStrings {
+        public void Run() {
+            string primeiroNome = "Nelson";
+            string ultimoNome = "Silva";
+            int idade = 21;
+
+            // Console.WriteLine("Nome: " + primeiroNome + " " + ultimoNome + "\nIdade: " + idade);
+            // Console.WriteLine("Nome: {0} {1}\nIdade: {2}", primeiroNome, ultimoNome, idade);
+            Console.WriteLine($"Nome: {primeiroNome} {ultimoNome}\nIdade: {idade}");
+        }
+    }
+}
