4173e029ea1306f9a2016ab5e5aca30f73555cab
Nelson Silva <devwop@outlook.com>
2016-08-21 04:39:32 +0100

    [Programar em C#/04-Operadores-Aritmeticos.cs] Add

---
+++ Programar em C#/04-Operadores-Aritmeticos.cs
@@ -0,0 +1,15 @@
+﻿// Programar em Python #04 - Operadores Aritméticos
+
+using System;
+
+namespace Base {
+    class OperadoresAritmeticos {
+        public void Run() {
+            Console.WriteLine(10 + 10);
+            Console.WriteLine(10.5 - 5.5 - 4.5);
+            Console.WriteLine(-10 * 2);
+            Console.WriteLine(10 / 10);
+            Console.WriteLine(8 % 2);
+        }
+    }
+}
