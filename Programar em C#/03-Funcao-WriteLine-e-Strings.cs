63516a1cfa8092dacdfde5922f6812f937b7625b
Nelson Silva <devwop@outlook.com>
2016-08-21 04:39:02 +0100

    [Programar em C#/03-Funcao-WriteLine-e-Strings.cs] Add

---
+++ Programar em C#/03-Funcao-WriteLine-e-Strings.cs
@@ -0,0 +1,13 @@
+﻿// Programar em Python #03 - Função WriteLine() e Strings
+
+using System;
+
+namespace Base {
+    class FuncaoWriteLineEStrings {
+        public void Run() {
+            Console.Write("Estamos a usar a função Write() e eu sou uma string!");
+            Console.WriteLine("Estamos a usar a função WriteLine() e eu continuo a ser uma string!");
+            Console.WriteLine("Estamos a usar novamente a função WriteLine() e eu continuo a ser uma string!");
+        }
+    }
+}
