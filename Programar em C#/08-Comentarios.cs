33ade781436105090d9ffde78d005d7cff768289
Nelson Silva <devwop@outlook.com>
2016-08-21 04:42:51 +0100

    [Programar em C#/08-Comentarios.cs] Add

---
+++ Programar em C#/08-Comentarios.cs
@@ -0,0 +1,20 @@
+﻿// Programar em Python #08 - Comentários
+
+using System;
+
+namespace Base {
+    class Comentarios {
+        public void Run() {
+            // Eu sou um comentário!
+
+            Console.WriteLine("Eu sou uma string!");
+
+            /* Eu também sou um comentário!
+             * Eu faço parte deste mesmo comentário!
+             * Continuo a fazer parte dele!
+             */
+
+            Console.WriteLine("Eu continuo a ser uma string!");
+        }
+    }
+}
