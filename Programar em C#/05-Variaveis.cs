d1e5d72498d97b0f97615d5619e4b9549414214d
Nelson Silva <devwop@outlook.com>
2016-08-21 04:39:57 +0100

    [Programar em C#/05-Variaveis.cs] Add

---
+++ Programar em C#/05-Variaveis.cs
@@ -0,0 +1,21 @@
+﻿// Programar em Python #05 - Variáveis
+
+using System;
+
+namespace Base {
+    class Variaveis {
+        public void Run() {
+            int _int = 10;
+            double _double = 10.5;
+            string _string = "Eu sou uma string!";
+            char _char = 'c';
+            bool _bool = true;
+
+            Console.WriteLine(_int);
+            Console.WriteLine(_double);
+            Console.WriteLine(_string);
+            Console.WriteLine(_char);
+            Console.WriteLine(_bool);
+        }
+    }
+}
