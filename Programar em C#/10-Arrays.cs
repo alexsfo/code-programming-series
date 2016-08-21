85e05b25da42f4e06390dcf468d7e2054a59c358
Nelson Silva <devwop@outlook.com>
2016-08-21 04:43:58 +0100

    [Programar em C#/10-Arrays.cs] Add

---
+++ Programar em C#/10-Arrays.cs
@@ -0,0 +1,29 @@
+﻿// Programar em Python #10 - Arrays
+
+using System;
+
+namespace Base {
+    class Arrays {
+        public void Run() {
+            /* string[] cores = new string[5];
+
+            cores[0] = "Azul";
+            cores[1] = "Verde";
+            cores[2] = "Amarelo";
+            cores[3] = "Vermelho";
+            cores[4] = "Laranja"; */
+
+            string[] cores = new string[] {
+                "Azul",
+                "Verde",
+                "Amarelo",
+                "Vermelho",
+                "Laranja"
+            };
+
+            Console.WriteLine($"Número de cores: {cores.Length}");
+            Console.WriteLine($"Primeira cor: {cores[0]}");
+            Console.WriteLine($"Última cor: {cores[cores.Length - 1]}");
+        }
+    }
+}
