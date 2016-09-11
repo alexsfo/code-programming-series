75aa3aaa0c3e9d178f8197ed0f7b99050d1467c7
Nelson Silva <devwop@outlook.com>
2016-09-11 17:22:17 +0100

    [Programar em C#/14-Operador-Ternario.cs] Add

---
+++ Programar em C#/14-Operador-Ternario.cs
@@ -0,0 +1,23 @@
+﻿// Programar em Python #14 - Operador Ternário
+
+using System;
+
+namespace Base {
+    class OperadorTernario {
+        // (condição) ? condição for verdadeira : condição for falsa;
+
+        public void Run() {
+            int idade = 21;
+            // bool eMaiorDeIdade;
+
+            /* if (idade >= 18)
+                eMaiorDeIdade = true;
+            else
+                eMaiorDeIdade = false; */
+
+            // eMaiorDeIdade = (idade >= 18) ? true : false;
+
+            Console.WriteLine($"É maior de idade: {((idade >= 18) ? true : false)}");
+        }
+    }
+}
