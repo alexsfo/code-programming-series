705fe42b98cab888e241b1ea9e9c73f2e9fc57c8
Nelson Silva <devwop@outlook.com>
2016-09-11 17:27:26 +0100

    [Programar em C#/18-Ciclo-while-e-do-while.cs] Add

---
+++ Programar em C#/18-Ciclo-while-e-do-while.cs
@@ -0,0 +1,23 @@
+﻿// Programar em Python #18 - Ciclo while e do while
+
+using System;
+
+namespace Base {
+    class CicloWhileEDoWhile {
+        public void Run() {
+            int contador = 1;
+
+            while (contador <= 10) {
+                Console.WriteLine($"[while] Contador: {contador}");
+                contador++;
+            }
+
+            contador = 1;
+
+            do {
+                Console.WriteLine($"[do while] Contador: {contador}");
+                contador++;
+            } while (contador <= 10);
+        }
+    }
+}
