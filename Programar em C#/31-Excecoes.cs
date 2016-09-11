7199f810041200218fdcb3080ebc4a706e6ba558
Nelson Silva <devwop@outlook.com>
2016-09-11 17:51:04 +0100

    [Programar em C#/31-Excecoes.cs] Add

---
+++ Programar em C#/31-Excecoes.cs
@@ -0,0 +1,24 @@
+﻿// Programar em Python #31 - Exceções
+
+using System;
+
+namespace Base {
+    class Excecoes {
+        public void Run() {
+            int[] numeros = new int[5] {
+                1, 2, 3, 4, 5
+            };
+
+            try {
+                Console.WriteLine($"Conteúdo do elemento: {numeros[5]}");
+            }
+            catch (Exception exception) {
+                // Console.WriteLine("O código presente no try não está a funcionar corretamente!");
+                Console.WriteLine(exception);
+            }
+            finally {
+                Console.WriteLine("Eu faço parte do finally e sou executado caso haja alguma exceção ou não!");
+            }
+        }
+    }
+}
