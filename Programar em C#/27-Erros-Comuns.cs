fc39593000083d711224fe6df3a8c731f21fdae8
Nelson Silva <devwop@outlook.com>
2016-09-11 17:42:17 +0100

    [Programar em C#/27-Erros-Comuns.cs] Add

---
+++ Programar em C#/27-Erros-Comuns.cs
@@ -0,0 +1,24 @@
+﻿// Programar em Python #27 - Erros Comuns
+
+using System;
+
+namespace Base {
+    class ErrosComuns {
+        public void Run() {
+            // Erro de compilação
+            Console.WriteLine("Eu sou uma string!");
+
+            // Erro de tempo de execução
+            int x = 10, y = 0;
+            Console.WriteLine($"Resultado da divisão: {x / y}");
+
+            // Erro de lógica
+            string primeiroNome = string.Empty, ultimoNome = string.Empty;
+
+            Console.Write("Insere o teu último nome: ");
+            ultimoNome = Console.ReadLine();
+
+            Console.WriteLine($"Nome: {primeiroNome} {ultimoNome}");
+        }
+    }
+}
