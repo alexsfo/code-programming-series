0734b12b13a405548bed3f7ee34bfce19930b28b
Nelson Silva <devwop@outlook.com>
2016-09-11 17:21:26 +0100

    [Programar em C#/13-Operadores-de-Comparacao.cs] Add

---
+++ Programar em C#/13-Operadores-de-Comparacao.cs
@@ -0,0 +1,17 @@
+﻿// Programar em Python #13 - Operadores de Comparação
+
+using System;
+
+namespace Base {
+    class OperadoresDeComparacao {
+        public void Run() {
+            int x = 10;
+            int y = 20;
+
+            if (x <= y || x == y)
+                Console.WriteLine("Esta condição é verdadeira!");
+            else
+                Console.WriteLine("Esta condição é falsa!");
+        }
+    }
+}
