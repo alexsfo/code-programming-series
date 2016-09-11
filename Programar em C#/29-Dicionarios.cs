77fba036cdd08921e2d2e6d8c6c714c49b7d09e1
Nelson Silva <devwop@outlook.com>
2016-09-11 17:43:31 +0100

    [Programar em C#/29-Dicionarios.cs] Add

---
+++ Programar em C#/29-Dicionarios.cs
@@ -0,0 +1,29 @@
+﻿// Programar em Python #29 - Dicionários
+
+using System;
+using System.Collections.Generic;
+
+namespace Base {
+    class Dicionarios {
+        public void Run() {
+            Dictionary<string, int> pessoas = new Dictionary<string, int>() {
+                { "Nelson Silva", 21 },
+                { "Maria Celeste", 37 }
+            };
+
+            pessoas.Add("Pedro Henrique", 52);
+            pessoas.Add("Raquel Soares", 68);
+
+            pessoas["Pedro Henrique"] = 100;
+            pessoas.Remove("Maria Celeste");
+            pessoas.Clear();
+
+            Console.WriteLine($"Número de pessoas: {pessoas.Count}\n");
+
+            foreach (KeyValuePair<string, int> pessoa in pessoas) {
+                Console.WriteLine($"Nome: {pessoa.Key}");
+                Console.WriteLine($"Idade: {pessoa.Value}");
+            }
+        }
+    }
+}
