34c881e8f992edb376ee65f011c204667e3442f3
Nelson Silva <devwop@outlook.com>
2016-09-11 17:39:51 +0100

    [Programar em C#/24-Argumentos-Chave.cs] Add

---
+++ Programar em C#/24-Argumentos-Chave.cs
@@ -0,0 +1,17 @@
+﻿// Programar em Python #24 - Argumentos Chave
+
+using System;
+
+namespace Base {
+    class ArgumentosChave {
+        public void Run() {
+            ImprimirABC(c: 1, a: 2, b: 3);
+        }
+
+        public void ImprimirABC(int a, int b, int c) {
+            Console.WriteLine($"Valor de a: {a}");
+            Console.WriteLine($"Valor de b: {b}");
+            Console.WriteLine($"Valor de c: {c}");
+        }
+    }
+}
