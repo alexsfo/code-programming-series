2ed5a21c80d2edcbe42c995168681b48a8bd0a5f
Nelson Silva <devwop@outlook.com>
2016-09-11 17:37:09 +0100

    [Programar em C#/22-Argumentos.cs] Add

---
+++ Programar em C#/22-Argumentos.cs
@@ -0,0 +1,17 @@
+﻿// Programar em Python #22 - Argumentos
+
+using System;
+
+namespace Base {
+    class Argumentos {
+        public void Run() {
+            DadosPessoais("Maria Celeste", 37, "Inglesa");
+        }
+
+        public void DadosPessoais(string nome, int idade, string nacionalidade) {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Idade: {idade}");
+            Console.WriteLine($"Nacionalidade: {nacionalidade}");
+        }
+    }
+}
