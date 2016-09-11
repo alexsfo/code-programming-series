38bccc406395ad7d8c6d0afc9dbd230ba315b39e
Nelson Silva <devwop@outlook.com>
2016-09-11 17:26:06 +0100

    [Programar em C#/16-Ciclo-for.cs] Add

---
+++ Programar em C#/16-Ciclo-for.cs
@@ -0,0 +1,24 @@
+﻿// Programar em Python #16 - Ciclo for
+
+using System;
+
+namespace Base {
+    class CicloFor {
+        public void Run() {
+            string[] materialEscolar = new string[6] {
+                "Mochila", // 0
+                "Estojo", // 1
+                "Lápis", // 2
+                "Borracha", // 3
+                "Afia", // 4
+                "Tesoura" // 5
+            };
+
+            /* Console.WriteLine(materialEscolar[0]);
+            Console.WriteLine(materialEscolar[1]); */
+
+            for (int indice = 0; indice < materialEscolar.Length; indice++)
+                Console.WriteLine($"materialEscolar[{indice}]: {materialEscolar[indice]}");
+        }
+    }
+}
