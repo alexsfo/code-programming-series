e7ce5a90b0e38f8dbc5c0e083d73a66d9d363229
Nelson Silva <devwop@outlook.com>
2016-09-11 17:28:03 +0100

    [Programar em C#/20-Funcoes.cs] Add

---
+++ Programar em C#/20-Funcoes.cs
@@ -0,0 +1,17 @@
+﻿// Programar em Python #20 - Funções
+
+using System;
+
+namespace Base {
+    class Funcoes {
+        public void Run() {
+            DadosPessoais();
+        }
+
+        public void DadosPessoais() {
+            Console.WriteLine("Nome: Nelson Silva");
+            Console.WriteLine("Idade: 21");
+            Console.WriteLine("Nacionalidade: Portuguesa");
+        }
+    }
+}
