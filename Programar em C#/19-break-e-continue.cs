1bbbfaf5b673c535baa0651c95a7f72928969237
Nelson Silva <devwop@outlook.com>
2016-09-11 17:27:51 +0100

    [Programar em C#/19-break-e-continue.cs] Add

---
+++ Programar em C#/19-break-e-continue.cs
@@ -0,0 +1,35 @@
+﻿// Programar em Python #19 - break e continue
+
+using System;
+using System.Collections.Generic;
+
+namespace Base {
+    class BreakEContinue {
+        public void Run() {
+            int contador = 0;
+            List<string> animais = new List<string>() {
+                "Cão",
+                "Gato",
+                "Galinha",
+                "Coelho",
+                "Leão"
+            };
+
+            foreach (string animal in animais) {
+                Console.WriteLine($"Animal: {animal}");
+
+                if (animal == "Galinha")
+                    break;
+            }
+
+            while (contador < 10) {
+                contador++;
+
+                if (contador == 5)
+                    continue;
+
+                Console.WriteLine($"Contador: {contador}");
+            }
+        }
+    }
+}
