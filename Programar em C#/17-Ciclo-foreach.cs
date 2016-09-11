702a050bc4dd4143b06827ff20ef8780332e1108
Nelson Silva <devwop@outlook.com>
2016-09-11 17:26:20 +0100

    [Programar em C#/17-Ciclo-foreach.cs] Add

---
+++ Programar em C#/17-Ciclo-foreach.cs
@@ -0,0 +1,27 @@
+﻿// Programar em Python #17 - Ciclo foreach
+
+using System;
+using System.Collections.Generic;
+
+namespace Base {
+    class CicloForeach {
+        public void Run() {
+            string organizacao = "Caffeine Algorithm";
+            List<string> paises = new List<string>() {
+                "Portugal",
+                "Brasil",
+                "Espanha",
+                "França",
+                "Itália",
+                "Austrália",
+                "Índia"
+            };
+
+            foreach (char carater in organizacao)
+                Console.WriteLine($"Carater: {carater}");
+
+            foreach (string pais in paises)
+                Console.WriteLine($"País: {pais}");
+        }
+    }
+}
