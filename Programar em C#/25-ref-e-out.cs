04a0a6e08f0f58bd2a426ae75d97f5a9779dc4ff
Nelson Silva <devwop@outlook.com>
2016-09-11 17:40:21 +0100

    [Programar em C#/25-ref-e-out.cs] Add

---
+++ Programar em C#/25-ref-e-out.cs
@@ -0,0 +1,27 @@
+﻿// Programar em Python #25 - ref e out
+
+using System;
+
+namespace Base {
+    class RefEOut {
+        public void Run() {
+            int numero = 0;
+
+            Console.WriteLine($"Número antes da função: {numero}");
+            Out(out numero);
+            Console.WriteLine($"Número depois da função: {numero}");
+        }
+
+        public void Normal(int numero) {
+            numero = 1;
+        }
+
+        public void Ref(ref int numero) {
+            numero = 2;
+        }
+
+        public void Out(out int numero) {
+            numero = 3;
+        }
+    }
+}
