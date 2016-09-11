d3e5700171e1b45878b13af0c0c21893b03a1b3f
Nelson Silva <devwop@outlook.com>
2016-09-11 17:25:44 +0100

    [Programar em C#/15-Switch.cs] Add

---
+++ Programar em C#/15-Switch.cs
@@ -0,0 +1,26 @@
+﻿// Programar em Python #15 - Switch
+
+using System;
+
+namespace Base {
+    class Switch {
+        public void Run() {
+            char caso = 'D';
+
+            switch (caso) {
+                case 'A':
+                    Console.WriteLine("O caso A existe!");
+                    break;
+                case 'B':
+                    Console.WriteLine("O caso B existe!");
+                    break;
+                case 'C':
+                    Console.WriteLine("O caso C existe!");
+                    break;
+                default:
+                    Console.WriteLine($"O caso {caso} não existe!");
+                    break;
+            }
+        }
+    }
+}
