587f2cb514c5d28d7f30d86df7d2fe6a86478933
Nelson Silva <devwop@outlook.com>
2016-09-11 17:51:37 +0100

    [Programar em C#/32-Classes-e-Objetos.cs] Add

---
+++ Programar em C#/32-Classes-e-Objetos.cs
@@ -0,0 +1,21 @@
+﻿// Programar em Python #32 - Classes e Objetos
+
+using System;
+
+namespace Base {
+    class ClassesEObjetos {
+        int vidas = 5;
+
+        public void Ataque() {
+            Console.WriteLine("Eu fui atacado e perdi uma vida!");
+            vidas -= 1;
+        }
+
+        public void VerificarVida() {
+            if (vidas <= 0)
+                Console.WriteLine("Eu estou morto porque não tenho mais vidas!");
+            else
+                Console.WriteLine($"Eu ainda estou em combate e tenho {vidas} vidas!");
+        }
+    }
+}
