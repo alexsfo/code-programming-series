797f88b8f4726edf6e38092e6281ff26e51d00f4
Nelson Silva <devwop@outlook.com>
2016-09-11 17:37:22 +0100

    [Programar em C#/23-Argumentos-Padrao.cs] Add

---
+++ Programar em C#/23-Argumentos-Padrao.cs
@@ -0,0 +1,18 @@
+﻿// Programar em Python #23 - Argumentos Padrão
+
+using System;
+
+namespace Base {
+    class ArgumentosPadrao {
+        public void Run() {
+            EstadoDaPorta(false);
+        }
+
+        public void EstadoDaPorta(bool estado = true) {
+            if (estado)
+                Console.WriteLine("A porta está aberta!");
+            else
+                Console.WriteLine("A porta está fechada!");
+        }
+    }
+}
