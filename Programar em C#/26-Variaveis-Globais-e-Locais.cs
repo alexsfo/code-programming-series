14afd9f2dc31c259661127190d5070ad675c60ea
Nelson Silva <devwop@outlook.com>
2016-09-11 17:40:43 +0100

    [Programar em C#/26-Variaveis-Globais-e-Locais.cs] Add

---
+++ Programar em C#/26-Variaveis-Globais-e-Locais.cs
@@ -0,0 +1,19 @@
+﻿// Programar em Python #26 - Variáveis Globais e Locais
+
+using System;
+
+namespace Base {
+    class VariaveisGlobaisELocais {
+        string acesso = "Global";
+
+        public void Run() {
+            MudarAcesso();
+            Console.WriteLine($"Acesso no exterior da função: {acesso}");
+        }
+
+        public void MudarAcesso() {
+            string acesso = "Local";
+            Console.WriteLine($"Acesso no interior da função: {acesso}");
+        }
+    }
+}
