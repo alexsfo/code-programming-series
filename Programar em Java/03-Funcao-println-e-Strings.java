e425957fec9e74f06c2825e3d666125333fb6074
Nelson Silva <devwop@outlook.com>
2016-09-24 16:57:54 +0100

    [Programar em Java/03-Funcao-println-e-Strings.java] Add

---
+++ Programar em Java/03-Funcao-println-e-Strings.java
@@ -0,0 +1,11 @@
+// Programar em Java #03 - Função println() e Strings
+
+package com.caffeinealgorithm.programaremjava;
+
+public class FuncaoPrintlnEStrings {
+    public void Run() {
+        System.out.print("Estamos a usar o método print() e eu sou uma string!");
+        System.out.println("Estamos a usar o método println() e eu continuo a ser uma string!");
+        System.out.println("Estamos a usar novamente o método println() e eu continuo a ser uma string!");
+    }
+}
