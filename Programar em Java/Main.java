2fa68384b90c3d90d0edc0e4cf3dc5c2015f1496
Nelson Silva <devwop@outlook.com>
2016-09-24 16:56:48 +0100

    [Programar em Java/Main.java] Add

---
+++ Programar em Java/Main.java
@@ -0,0 +1,8 @@
+package com.caffeinealgorithm.programaremjava;
+
+public class Main {
+    public static void main(String[] args) {
+        ClasseDoVideoEmQuestao video = new ClasseDoVideoEmQuestao();
+        video.Run();
+    }
+}
