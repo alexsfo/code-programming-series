2a0294d691c37d9792e43c8b5048352a65ceff00
Nelson Silva <devwop@outlook.com>
2016-11-03 22:49:59 +0000

    [Programar em C#/33-Construtores.cs] Add

---
+++ Programar em C#/33-Construtores.cs
@@ -0,0 +1,28 @@
+﻿// Programar em C# #33 - Construtores
+
+using System;
+
+namespace Base {
+    class Construtores {
+        string primeiroNome = string.Empty, ultimoNome = string.Empty;
+        int idade = 0;
+
+        public Construtores(string primeiroNome, string ultimoNome, int idade) {
+            this.primeiroNome = primeiroNome;
+            this.ultimoNome = ultimoNome;
+            this.idade = idade;
+        }
+
+        public void Informacao() {
+            Console.WriteLine($"Nome: {primeiroNome} {ultimoNome}");
+            Console.WriteLine($"Idade: {idade}");
+        }
+
+        public void VerificarEntrada() {
+            if (idade >= 18)
+                Console.WriteLine("Esta pessoa pode entrar no local porque tem mais de 18 anos!");
+            else
+                Console.WriteLine("Esta pessoa não pode entrar no local porque tem menos de 18 anos!");
+        }
+    }
+}
