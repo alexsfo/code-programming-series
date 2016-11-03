4ea3bdba37bc14662e601c17be0e1e8cfe945546
Nelson Silva <devwop@outlook.com>
2016-11-03 22:55:52 +0000

    [Programar em C#/40-struct.cs] Add

---
+++ Programar em C#/40-struct.cs
@@ -0,0 +1,35 @@
+﻿// Programar em C# #40 - struct
+
+using System;
+
+namespace Base {
+    class Struct {
+        public void Run() {
+            var estrutura = new Estrutura();
+            estrutura.Numero = 200;
+        }
+
+        struct Estrutura : IEstrutura {
+            private int numero;
+
+            public int Numero {
+                set {
+                    if (value >= 0 && value <= 100) {
+                        numero = value;
+                        ImprimirNumero();
+                    }
+                    else
+                        Console.WriteLine("O número precisa de ser >= 0 e <= 100!");
+                }
+            }
+
+            public void ImprimirNumero() {
+                Console.WriteLine($"Número: {numero}");
+            }
+        }
+
+        interface IEstrutura {
+            void ImprimirNumero();
+        }
+    }
+}
