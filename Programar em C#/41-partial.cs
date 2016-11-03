f8b0c84e252c1b9a8f6fa52a60136e5780728488
Nelson Silva <devwop@outlook.com>
2016-11-03 22:56:26 +0000

    [Programar em C#/41-partial.cs] Add

---
+++ Programar em C#/41-partial.cs
@@ -0,0 +1,31 @@
+﻿// Programar em C# #41 - partial
+
+using System;
+
+namespace Base {
+    class Partial {
+        public void Run() {
+            var classe = new Classe();
+            classe.Informacao();
+        }
+
+        partial class Classe {
+            private string primeiroNome = "Nelson";
+            partial void _Informacao();
+
+            public void Informacao() {
+                _Informacao();
+            }
+        }
+
+        partial class Classe {
+            private string ultimoNome = "Silva";
+            private int idade = 21;
+
+            partial void _Informacao() {
+                Console.WriteLine($"Nome: {primeiroNome} {ultimoNome}");
+                Console.WriteLine($"Idade: {idade}");
+            }
+        }
+    }
+}
