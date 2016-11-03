1f1c9d0e7c68e12b58d8850bceb32212e0c6a0e7
Nelson Silva <devwop@outlook.com>
2016-11-03 22:54:43 +0000

    [Programar em C#/37-Heranca.cs] Add

---
+++ Programar em C#/37-Heranca.cs
@@ -0,0 +1,31 @@
+﻿// Programar em C# #37 - Herança
+
+using System;
+
+namespace Base {
+    class Heranca {
+        public void Run() {
+            var pessoa = new Filho();
+            pessoa.Informacao();
+            pessoa.ComidaPreferida();
+        }
+    }
+
+    class Pai {
+        protected string ultimoNome = "Silva";
+
+        public void ComidaPreferida() {
+            Console.WriteLine("A minha comida preferida é arroz de marisco!");
+        }
+    }
+
+    class Filho : Pai {
+        private string primeiroNome = "Nelson";
+        private int idade = 21;
+
+        public void Informacao() {
+            Console.WriteLine($"Nome: {primeiroNome} {ultimoNome}");
+            Console.WriteLine($"Idade: {idade}");
+        }
+    }
+}
