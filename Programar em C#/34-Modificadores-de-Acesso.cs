a65d8412c6a5ed9760c52ed7fd669dab5c065bec
Nelson Silva <devwop@outlook.com>
2016-11-03 22:52:52 +0000

    [Programar em C#/34-Modificadores-de-Acesso.cs] Add

---
+++ Programar em C#/34-Modificadores-de-Acesso.cs
@@ -0,0 +1,11 @@
+﻿// Programar em C# #34 - Modificadores de Acesso
+
+using System;
+
+namespace Base {
+    class ModificadoresDeAcesso {
+        public void Run() {
+            Console.WriteLine("Estamos aprender a trabalhar com modificadores de acesso!");
+        }
+    }
+}
