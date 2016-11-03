92db52795767e38ed43f3ef5f7c2eb8d786be395
Nelson Silva <devwop@outlook.com>
2016-11-03 22:53:18 +0000

    [Programar em C#/35-Mais-sobre-as-Excecoes.cs] Add

---
+++ Programar em C#/35-Mais-sobre-as-Excecoes.cs
@@ -0,0 +1,23 @@
+﻿// Programar em C# #35 - Mais sobre as Exceções
+
+using System;
+
+namespace Base {
+    class MaisSobreAsExcecoes {
+        private Exception stringVazia = new Exception("Não se pode verificar uma string que esteja vazia!");
+
+        public void Run() {
+            try {
+                VerificarString("");
+            }
+            catch (Exception exception) {
+                Console.WriteLine(exception);
+            }
+        }
+
+        private void VerificarString(string _string) {
+            if (_string == string.Empty)
+                throw stringVazia;
+        }
+    }
+}
