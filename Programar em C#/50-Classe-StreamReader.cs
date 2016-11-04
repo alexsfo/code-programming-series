99ca555ba7cee953d8b7b9be15e8270e97b3cfaa
Nelson Silva <devwop@outlook.com>
2016-11-04 23:29:58 +0000

    [Programar em C#/50-Classe-StreamReader.cs] Add

---
+++ Programar em C#/50-Classe-StreamReader.cs
@@ -0,0 +1,21 @@
+﻿// Programar em C# #50 - Classe StreamReader
+
+using System;
+using System.IO;
+
+namespace Base {
+    class ClasseStreamReader {
+        private StreamReader lerFicheiro;
+
+        public void Run() {
+            using (lerFicheiro = new StreamReader("Ficheiro.txt")) {
+                var linha = string.Empty;
+
+                /* while ((linha = lerFicheiro.ReadLine()) != null)
+                    Console.WriteLine(linha); */
+
+                Console.WriteLine(lerFicheiro.ReadToEnd());
+            }
+        }
+    }
+}
