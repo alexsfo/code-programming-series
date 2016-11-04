c8eb41cb4755324fb730a3e9812b657a68339c04
Nelson Silva <devwop@outlook.com>
2016-11-04 23:29:46 +0000

    [Programar em C#/49-Classe-StreamWriter.cs] Add

---
+++ Programar em C#/49-Classe-StreamWriter.cs
@@ -0,0 +1,26 @@
+﻿// Programar em C# #49 - Classe StreamWriter
+
+using System.Collections.Generic;
+using System.IO;
+
+namespace Base {
+    class ClasseStreamWriter {
+        private StreamWriter escreverFicheiro;
+        private List<string> paises = new List<string>() {
+            "Portugal",
+            "Brasil",
+            "Espanha",
+            "França",
+            "Itália",
+            "Austrália",
+            "Índia"
+        };
+
+        public void Run() {
+            using (escreverFicheiro = new StreamWriter("Ficheiro.txt")) {
+                foreach (var pais in paises)
+                    escreverFicheiro.WriteLine(pais);
+            }
+        }
+    }
+}
