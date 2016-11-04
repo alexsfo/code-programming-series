016ff0f39232b43db81aba47d89afe2f932d5120
Nelson Silva <devwop@outlook.com>
2016-11-04 23:28:36 +0000

    [Programar em C#/43-delegate.cs] Add

---
+++ Programar em C#/43-delegate.cs
@@ -0,0 +1,31 @@
+﻿// Programar em C# #43 - delegate
+
+using System;
+
+namespace Base {
+    class Delegate {
+        private delegate void ConjuntoDeMetodos(string nome);
+
+        public void Run() {
+            Console.Write("Insere o teu primeiro e último nome: ");
+            var nome = Console.ReadLine();
+
+            var conjuntoDeMetodos = new ConjuntoDeMetodos(ImprimirNome);
+            conjuntoDeMetodos += ImprimirNomeEmMaiusculas;
+            conjuntoDeMetodos += ImprimirNomeEmMinusculas;
+            conjuntoDeMetodos(nome);
+        }
+
+        public void ImprimirNome(string nome) {
+            Console.WriteLine($"Nome: {nome}");
+        }
+
+        public void ImprimirNomeEmMaiusculas(string nome) {
+            Console.WriteLine($"Nome em maiúsculas: {nome.ToUpper()}");
+        }
+
+        public void ImprimirNomeEmMinusculas(string nome) {
+            Console.WriteLine($"Nome em minúsculas: {nome.ToLower()}");
+        }
+    }
+}
