e33fb85fb67b8549c66e9be035df59bc50cac583
Nelson Silva <devwop@outlook.com>
2016-11-04 23:29:25 +0000

    [Programar em C#/47-Classe-Random.cs] Add

---
+++ Programar em C#/47-Classe-Random.cs
@@ -0,0 +1,17 @@
+﻿// Programar em C# #47 - Classe Random
+
+using System;
+
+namespace Base {
+    class ClasseRandom {
+        private Random aleatorio = new Random();
+
+        public void Run() {
+            Console.WriteLine($"Número aleatório até 20: {aleatorio.Next(21)}");
+            Console.WriteLine($"Número aleatório entre 20 e 40: {aleatorio.Next(20, 41)}");
+
+            for (int indice = 1; indice <= 10; indice++)
+                Console.WriteLine($"Número aleatório #{indice}: {aleatorio.Next()}");
+        }
+    }
+}
