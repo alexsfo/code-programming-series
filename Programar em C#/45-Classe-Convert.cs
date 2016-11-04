57b0d082efba7d36cb26dde5670c74a6bb31d8f1
Nelson Silva <devwop@outlook.com>
2016-11-04 23:29:05 +0000

    [Programar em C#/45-Classe-Convert.cs] Add

---
+++ Programar em C#/45-Classe-Convert.cs
@@ -0,0 +1,28 @@
+﻿// Programar em C# #45 - Classe Convert
+
+using System;
+
+namespace Base {
+    class ClasseConvert {
+        private string letra = "N", anoAtual = "2016";
+        private char _letra = ' ';
+        private int _anoAtual = 0;
+
+        public void Run() {
+            try {
+                _letra = Convert.ToChar(letra);
+                _anoAtual = Convert.ToInt32(anoAtual);
+            }
+            catch (Exception excecao) {
+                Console.WriteLine(excecao);
+            }
+            finally {
+                if (_letra != ' ')
+                    Console.WriteLine($"Letra: {_letra}");
+
+                if (_anoAtual != 0)
+                    Console.WriteLine($"Ano atual: {_anoAtual}");
+            }
+        }
+    }
+}
