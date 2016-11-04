8961d0991242dd4c6e56f6c3b9cd4979b420acf3
Nelson Silva <devwop@outlook.com>
2016-11-04 23:29:35 +0000

    [Programar em C#/48-Classe-File.cs] Add

---
+++ Programar em C#/48-Classe-File.cs
@@ -0,0 +1,20 @@
+﻿// Programar em C# #48 - Classe File
+
+using System;
+using System.IO;
+
+namespace Base {
+    class ClasseFile {
+        public void Run() {
+            // File.Create("File-Create.txt");
+            // File.Copy("File-Create.txt", "File-Copy.txt", true);
+            File.Delete("File-Move.txt");
+            // File.Move("File-Create.txt", "File-Move.txt");
+
+            if (File.Exists("File-Move.txt"))
+                Console.WriteLine("O ficheiro \"File-Move.txt\" existe!");
+            else
+                Console.WriteLine("O ficheiro \"File-Move.txt\" não existe!");
+        }
+    }
+}
