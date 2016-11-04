c3050d76235ca706fb47cd550dbbc6a6726a83d3
Nelson Silva <devwop@outlook.com>
2016-11-04 23:30:08 +0000

    [Programar em C#/51-Classe-Path.cs] Add

---
+++ Programar em C#/51-Classe-Path.cs
@@ -0,0 +1,17 @@
+﻿// Programar em C# #51 - Classe Path
+
+using System;
+using System.IO;
+
+namespace Base {
+    class ClassePath {
+        private const string Ficheiro = "Ficheiro.txt";
+
+        public void Run() {
+            Console.WriteLine($"Nome do ficheiro com a extensão: {Path.GetFileName(Ficheiro)}");
+            Console.WriteLine($"Nome do ficheiro sem a extensão: {Path.GetFileNameWithoutExtension(Ficheiro)}");
+            Console.WriteLine($"Extensão do ficheiro: {Path.GetExtension(Ficheiro)}");
+            Console.WriteLine($"Caminho absoluto até ao ficheiro: {Path.GetFullPath(Ficheiro)}");
+        }
+    }
+}
