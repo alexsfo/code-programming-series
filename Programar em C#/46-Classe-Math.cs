b3d58cf98fe3f37e24d6f86b6707866b3a65a055
Nelson Silva <devwop@outlook.com>
2016-11-04 23:29:15 +0000

    [Programar em C#/46-Classe-Math.cs] Add

---
+++ Programar em C#/46-Classe-Math.cs
@@ -0,0 +1,18 @@
+﻿// Programar em C# #46 - Classe Math
+
+using System;
+
+namespace Base {
+    class ClasseMath {
+        public void Run() {
+            Console.WriteLine($"Resultado do método Sin(): {Math.Sin(10.5)}");
+            Console.WriteLine($"Resultado do método Cos(): {Math.Cos(10.5)}");
+            Console.WriteLine($"Resultado do método Tan(): {Math.Tan(10.5)}");
+            Console.WriteLine($"Resultado do método Floor(): {Math.Floor(10.5)}");
+            Console.WriteLine($"Resultado do método Ceiling(): {Math.Ceiling(10.5)}");
+            Console.WriteLine($"Resultado do método Abs(): {Math.Abs(-10.5)}");
+            Console.WriteLine($"Resultado do método Sqrt(): {Math.Sqrt(10.5)}");
+            Console.WriteLine($"Resultado do método Pow(): {Math.Pow(2, 5)}"); // 2 * 2 * 2 * 2 * 2
+        }
+    }
+}
