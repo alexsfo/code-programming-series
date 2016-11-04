f0ed48926c18c484be8516b32da6cf6be63913c1
Nelson Silva <devwop@outlook.com>
2016-11-04 23:28:48 +0000

    [Programar em C#/44-is-e-as.cs] Add

---
+++ Programar em C#/44-is-e-as.cs
@@ -0,0 +1,22 @@
+﻿// Programar em C# #44 - is e as
+
+using System;
+
+namespace Base {
+    class IsEAs {
+        private string organizacao = "Caffeine Algorithm", nome = string.Empty;
+        private object _nome = "Nelson Silva";
+
+        public void Run() {
+            if (organizacao is string)
+                Console.WriteLine("O atributo \"organizacao\" é do tipo string!");
+            else
+                Console.WriteLine("O atributo \"organizacao\" não é do tipo string!");
+
+            nome = _nome as string;
+
+            if (nome is string)
+                Console.WriteLine($"Nome: {nome}");
+        }
+    }
+}
