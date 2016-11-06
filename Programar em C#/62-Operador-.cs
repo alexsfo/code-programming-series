7b03cf6ff61294f32c6bfcfee3cceb4b0db584d6
Nelson Silva <devwop@outlook.com>
2016-11-06 22:44:51 +0000

    [Programar em C#/62-Operador-.cs] Add

---
+++ Programar em C#/62-Operador-.cs
@@ -0,0 +1,37 @@
+﻿// Programar em C# #62 - Operador ??
+
+using System;
+
+namespace Base {
+    class OperadorCoalescenciaNula {
+        private string nome;
+
+        public string Nome {
+            get {
+                return nome ?? "O conteúdo é simplesmente nulo!";
+            }
+
+            set {
+                nome = value;
+            }
+        }
+
+        public void Run() {
+            Console.WriteLine(Nome);
+
+            Nome = "Nelson Silva";
+            Console.WriteLine($"Nome: {Nome}");
+
+            Nome = null;
+            Console.WriteLine(Nome);
+
+            VerificarTamanho(null);
+            VerificarTamanho("Coalescência");
+        }
+
+        private void VerificarTamanho(string palavra) {
+            if (palavra?.Length > 5)
+                Console.WriteLine($"\"{palavra}\": Esta palavra tem mais de 5 carateres!");
+        }
+    }
+}
