718a8ba1730b994fb18bc2175b6380607cf471a4
Nelson Silva <devwop@outlook.com>
2016-11-06 22:44:17 +0000

    [Programar em C#/60-Metodos-II.cs] Add

---
+++ Programar em C#/60-Metodos-II.cs
@@ -0,0 +1,29 @@
+﻿// Programar em C# #60 - Métodos II
+
+using System;
+
+namespace Base {
+    class MetodosII {
+        private string loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consectetur.";
+
+        public void Run() {
+            // IndexOf()
+            var indice = 0;
+
+            while ((indice = loremIpsum.IndexOf('i', indice)) != -1) {
+                Console.WriteLine(loremIpsum.Substring(indice));
+                indice++;
+            }
+
+            // Trim()
+            Console.Write("Insere o teu primeiro nome: ");
+            var primeiroNome = Console.ReadLine();
+
+            Console.Write("Insere o teu último nome: ");
+            var ultimoNome = Console.ReadLine();
+
+            Console.WriteLine($"Nome (sem o uso do método Trim()): {primeiroNome} {ultimoNome}");
+            Console.WriteLine($"Nome (com o uso do método Trim()): {primeiroNome.Trim()} {ultimoNome.Trim()}");
+        }
+    }
+}
