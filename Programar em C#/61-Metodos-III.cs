1570d0b3519353b5271d4e0ce552091c9beb59ff
Nelson Silva <devwop@outlook.com>
2016-11-06 22:44:28 +0000

    [Programar em C#/61-Metodos-III.cs] Add

---
+++ Programar em C#/61-Metodos-III.cs
@@ -0,0 +1,22 @@
+﻿// Programar em C# #61 - Métodos III
+
+using System;
+
+namespace Base {
+    class MetodosIII {
+        private string abecedario = "ABCDEFGHIJKLMNOPQRSTUVXWYZ", nome = "Nelson | Silva";
+
+        public void Run() {
+            // Remove()
+            Console.WriteLine($"Abecedário até à letra N: {abecedario.Remove(14)}");
+
+            var indiceX = nome.IndexOf(' ');
+            var indiceY = nome.IndexOf(' ', indiceX + 1);
+            Console.WriteLine($"Nome: {nome.Remove(indiceX, indiceY - indiceX)}");
+
+            // Replace()
+            var lingProgPreferida = "A minha linguagem de programação preferida já foi o C#!";
+            Console.WriteLine(lingProgPreferida.Replace("já foi", "é"));
+        }
+    }
+}
