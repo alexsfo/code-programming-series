a59cc519b5c5fcf17b3428fd67b77ff572fb62da
Nelson Silva <devwop@outlook.com>
2016-11-06 22:42:55 +0000

    [Programar em C#/55-Classe-ArrayList.cs] Add

---
+++ Programar em C#/55-Classe-ArrayList.cs
@@ -0,0 +1,29 @@
+﻿// Programar em C# #55 - Classe ArrayList
+
+using System;
+using System.Collections;
+
+namespace Base {
+    class ClasseArrayList {
+        private ArrayList materiaisEscolares = new ArrayList() {
+            "Mochila",
+            "Estojo",
+            "Lápis",
+            "Borracha"
+        };
+
+        public void Run() {
+            materiaisEscolares.Add("Afia");
+            materiaisEscolares.Add("Tesoura");
+
+            materiaisEscolares.Remove("Mochila");
+            materiaisEscolares.Reverse();
+            materiaisEscolares.Clear();
+
+            Console.WriteLine($"Número de materiais escolares: {materiaisEscolares.Count}");
+
+            foreach (var materialEscolar in materiaisEscolares)
+                Console.WriteLine($"Material escolar: {materialEscolar}");
+        }
+    }
+}
