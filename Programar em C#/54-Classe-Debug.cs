901bc69b34d4f6e210b53454082ebcc2cab7530c
Nelson Silva <devwop@outlook.com>
2016-11-06 22:42:45 +0000

    [Programar em C#/54-Classe-Debug.cs] Add

---
+++ Programar em C#/54-Classe-Debug.cs
@@ -0,0 +1,20 @@
+﻿// Programar em C# #54 - Classe Debug
+
+using System.Diagnostics;
+
+namespace Base {
+    class ClasseDebug {
+        private string[] cores = new string[] {
+            "Azul",
+            "Verde",
+            "Amarelo",
+            "Vermelho",
+            "Laranja"
+        };
+
+        public void Run() {
+            for (int indice = 0; indice < cores.Length; indice++)
+                Debug.WriteLine($"cores[{indice}]: {cores[indice]}");
+        }
+    }
+}
