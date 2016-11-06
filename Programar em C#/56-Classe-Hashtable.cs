356382fa061a0cd79392d6b2f6d0c01e022da408
Nelson Silva <devwop@outlook.com>
2016-11-06 22:43:18 +0000

    [Programar em C#/56-Classe-Hashtable.cs] Add

---
+++ Programar em C#/56-Classe-Hashtable.cs
@@ -0,0 +1,29 @@
+﻿// Programar em C# #56 - Classe Hashtable
+
+using System;
+using System.Collections;
+
+namespace Base {
+    class ClasseHashtable {
+        private Hashtable pessoas = new Hashtable() {
+            { "Nelson Silva", 21 },
+            { "Maria Celeste", 37 }
+        };
+
+        public void Run() {
+            pessoas.Add("Pedro Henrique", 52);
+            pessoas.Add("Raquel Soares", 68);
+
+            pessoas["Pedro Henrique"] = 100;
+            pessoas.Remove("Maria Celeste");
+            pessoas.Clear();
+
+            Console.WriteLine($"Número de pessoas: {pessoas.Count}");
+
+            foreach (DictionaryEntry pessoa in pessoas) {
+                Console.WriteLine($"Nome: {pessoa.Key}");
+                Console.WriteLine($"Idade: {pessoa.Value}");
+            }
+        }
+    }
+}
