12f55a758c4903e78503e0956d5f6898d9c7b5b6
Nelson Silva <devwop@outlook.com>
2016-11-06 22:43:46 +0000

    [Programar em C#/58-Classe-Queue.cs] Add

---
+++ Programar em C#/58-Classe-Queue.cs
@@ -0,0 +1,32 @@
+﻿// Programar em C# #58 - Classe Queue
+
+using System;
+using System.Collections.Generic;
+
+namespace Base {
+    class ClasseQueue {
+        private Queue<int> fila = new Queue<int>();
+        private const int Multiplicador = 10;
+        private int numero = 1;
+
+        public void Run() {
+            for (int indice = 1; indice <= 5; indice++) {
+                fila.Enqueue(numero);
+                numero *= Multiplicador;
+            }
+
+            ImprimirFila();
+
+            Console.WriteLine($"\nA remover o número {fila.Dequeue()} da fila com o método Dequeue()!\n");
+
+            ImprimirFila();
+
+            Console.WriteLine($"\nO número que se encontra na parte superior da fila é o {fila.Peek()}!");
+        }
+
+        private void ImprimirFila() {
+            foreach (var numero in fila)
+                Console.WriteLine(numero);
+        }
+    }
+}
