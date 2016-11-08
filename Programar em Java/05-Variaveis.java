6145e195d480d1c8c657b3ee63a6e317a0d50db8
Nelson Silva <devwop@outlook.com>
2016-11-08 20:57:42 +0000

    [Programar em Java/05-Variaveis.java] Add

---
+++ Programar em Java/05-Variaveis.java
@@ -0,0 +1,19 @@
+// Programar em Java #05 - Variáveis
+
+package com.caffeinealgorithm.programaremjava;
+
+public class Variaveis {
+    public void Run() {
+        int _int = 10;
+        double _double = 10.5;
+        String _String = "Eu sou uma string!";
+        char _char = 'c';
+        boolean _boolean = false;
+
+        System.out.println(_int);
+        System.out.println(_double);
+        System.out.println(_String);
+        System.out.println(_char);
+        System.out.println(_boolean);
+    }
+}
