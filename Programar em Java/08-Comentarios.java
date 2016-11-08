b6c84935378f80e79e8e8f71583ed3745bf95782
Nelson Silva <devwop@outlook.com>
2016-11-08 20:58:59 +0000

    [Programar em Java/08-Comentarios.java] Add

---
+++ Programar em Java/08-Comentarios.java
@@ -0,0 +1,19 @@
+// Programar em Java #08 - Comentários
+
+package com.caffeinealgorithm.programaremjava;
+
+public class Comentarios {
+    public void Run() {
+        // Eu sou um comentário!
+
+        System.out.println("Eu sou uma string!");
+
+        /*
+            Eu também sou um comentário!
+            Eu faço parte deste mesmo comentário!
+            Continuo a fazer parte dele!
+        */
+
+        System.out.println("Eu continuo a ser uma string!");
+    }
+}
