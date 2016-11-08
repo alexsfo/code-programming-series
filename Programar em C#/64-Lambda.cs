108ea0c5a93402991c3e672f9f266723c43094f1
Nelson Silva <devwop@outlook.com>
2016-11-08 20:41:18 +0000

    [Programar em C#/64-Lambda.cs] Add

---
+++ Programar em C#/64-Lambda.cs
@@ -0,0 +1,42 @@
+﻿// Programar em C# #64 - Lambda
+
+using System;
+using System.Linq;
+
+namespace Base {
+    class Lambda {
+        private int[] numeros = new int[] {
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+        };
+
+        public void Run() {
+            /* Console.WriteLine($"Total de números pares: {ContarNumerosPares()}");
+            Console.WriteLine($"Total de números ímpares: {ContarNumerosImpares()}"); */
+
+            Console.WriteLine($"Total de números pares: {numeros.Count(numero => numero % 2 == 0)}");
+            Console.WriteLine($"Total de números ímpares: {numeros.Count(numero => numero % 2 != 0)}");
+        }
+
+        private int ContarNumerosPares() {
+            var contador = 0;
+
+            foreach (var numero in numeros) {
+                if (numero % 2 == 0)
+                    contador++;
+            }
+
+            return contador;
+        }
+
+        private int ContarNumerosImpares() {
+            var contador = 0;
+
+            foreach (var numero in numeros) {
+                if (numero % 2 != 0)
+                    contador++;
+            }
+
+            return contador;
+        }
+    }
+}
