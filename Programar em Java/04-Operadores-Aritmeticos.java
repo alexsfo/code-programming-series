4d60455359e3c48e13e663c1fb38c447e314b0eb
Nelson Silva <devwop@outlook.com>
2016-11-08 20:57:12 +0000

    [Programar em Java/04-Operadores-Aritmeticos.java] Add

---
+++ Programar em Java/04-Operadores-Aritmeticos.java
@@ -0,0 +1,13 @@
+// Programar em Java #04 - Operadores Aritméticos
+
+package com.caffeinealgorithm.programaremjava;
+
+public class OperadoresAritmeticos {
+    public void Run() {
+        System.out.println(10 + 10);
+        System.out.println(10.5 - 5.5 - 4.5);
+        System.out.println(-10 * 2);
+        System.out.println(10 / 2);
+        System.out.println(8 % 2);
+    }
+}
