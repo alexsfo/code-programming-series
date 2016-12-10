218fe487f59eee80b46e4332887885a66cafa919
Nelson Silva <devwop@outlook.com>
2016-12-10 18:37:48 +0000

    [Programar em Go/02-Funcao-Println-e-Strings.go] Add

---
+++ Programar em Go/02-Funcao-Println-e-Strings.go
@@ -0,0 +1,11 @@
+// Programar em Go #02 - Função Println e Strings
+
+package main;
+
+import "fmt";
+
+func main() {
+	fmt.Print("Estamos a usar a função Print() e eu sou uma string!");
+	fmt.Println("A Maria disse que o Miguel estava \"doente\"!");
+	fmt.Println("Caffeine", "Algorithm");
+}
