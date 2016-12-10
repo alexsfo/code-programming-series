086289701bd1b4cc962bcc3b32c524bb99d80ca6
Nelson Silva <devwop@outlook.com>
2016-12-10 18:25:15 +0000

    [Programar em Java/20-Funcoes.java] Add

---
+++ Programar em Java/20-Funcoes.java
@@ -0,0 +1,15 @@
+// Programar em Java #20 - Funções
+
+package com.caffeinealgorithm.programaremjava;
+
+public class Funcoes {
+    public void Run() {
+        DadosPessoais();
+    }
+
+    public void DadosPessoais() {
+        System.out.println("Nome: Nelson Silva");
+        System.out.println("Idade: 21");
+        System.out.println("Nacionalidade: Portuguesa");
+    }
+}
