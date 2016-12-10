95c0bdcfa836202c0ce90879606fc5b3bfb0a0ac
Nelson Silva <devwop@outlook.com>
2016-12-10 18:38:10 +0000

    [Programar em Go/03-Operadores-Aritmeticos.go] Add

---
+++ Programar em Go/03-Operadores-Aritmeticos.go
@@ -0,0 +1,13 @@
+// Programar em Go #03 - Operadores Aritméticos
+
+package main;
+
+import "fmt";
+
+func main() {
+	fmt.Println(10 + 10);
+	fmt.Println(10.5 - 5.5 - 5.0);
+	fmt.Println(-10 * -2);
+	fmt.Println(10 / 10);
+	fmt.Println(9 % 2);
+}
