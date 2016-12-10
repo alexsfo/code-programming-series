66469bb0437fa6b3e1b88e404b554dfe09b95901
Nelson Silva <devwop@outlook.com>
2016-12-10 18:39:08 +0000

    [Programar em Go/05-Operadores-de-Atribuicao.go] Add

---
+++ Programar em Go/05-Operadores-de-Atribuicao.go
@@ -0,0 +1,24 @@
+// Programar em Go #05 - Operadores de Atribuição
+
+package main;
+
+import "fmt";
+
+func main() {
+	x := 2;
+
+	x += 3;
+	fmt.Println("x = x + 3:", x);
+
+	x -= 2;
+	fmt.Println("x = x - 2:", x);
+
+	x *= 2;
+	fmt.Println("x = x * 2:", x);
+
+	x /= 2;
+	fmt.Println("x = x / 2:", x);
+
+	x %= 3;
+	fmt.Println("x = x % 3:", x);
+}
