e295ede61708cca34ec108e461ceaa66ba29ba73
Nelson Silva <devwop@outlook.com>
2016-12-10 18:27:34 +0000

    [Programar em Java/30-Modificadores-de-Acesso.java] Add

---
+++ Programar em Java/30-Modificadores-de-Acesso.java
@@ -0,0 +1,11 @@
+// Programar em Java #30 - Modificadores de Acesso
+
+package com.caffeinealgorithm.programaremjava;
+
+public class ModificadoresDeAcesso {
+    // private int numero = 100;
+
+    public void Run() {
+        System.out.println("Estamos aprender a trabalhar com modificadores de acesso!");
+    }
+}
