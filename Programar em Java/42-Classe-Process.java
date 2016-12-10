d443cf1a8afbd3eda64fd6380f237838081d758b
Nelson Silva <devwop@outlook.com>
2016-12-10 18:30:05 +0000

    [Programar em Java/42-Classe-Process.java] Add

---
+++ Programar em Java/42-Classe-Process.java
@@ -0,0 +1,27 @@
+// Programar em Java #42 - Classe Process
+
+package com.caffeinealgorithm.programaremjava;
+
+public class ClasseProcess {
+    private Process processo;
+
+    public void Run() {
+        try {
+            processo = new ProcessBuilder("notepad.exe").start();
+        }
+        catch (Exception excecao) {
+            System.out.println(excecao);
+        }
+
+        try {
+            if (processo.isAlive()) {
+                System.out.println("O processo \"notepad.exe\" está a ser executado e tal execução irá terminar em três segundos!");
+                Thread.sleep(3000);
+                processo.destroy();
+            }
+        }
+        catch (InterruptedException excecao) {
+            System.out.println(excecao);
+        }
+    }
+}
