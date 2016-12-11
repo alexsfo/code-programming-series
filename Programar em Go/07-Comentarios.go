92f7b21d0f4bc76f5f7ce34c35f24ec7b9f61870
Nelson Silva <devwop@outlook.com>
2016-12-11 20:13:41 +0000

    [Programar em Go/07-Comentarios.go] Add

---
+++ Programar em Go/07-Comentarios.go
@@ -0,0 +1,19 @@
+// Programar em Go #07 - Comentários
+
+package main;
+
+import "fmt";
+
+func main() {
+	// Eu sou um comentário!
+
+	fmt.Println("Eu sou uma string!");
+
+	/*
+		Eu também sou um comentário!
+		Eu faço parte deste mesmo comentário!
+		Continuo a fazer parte dele!
+	*/
+
+	fmt.Println("Eu continuo a ser uma string!");
+}
